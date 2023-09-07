835ee0cf68b94e1e4880d8d394281ca248e4e97f
Bilal Hussain <bilal.hussain@eurosofttech.co.uk>
2023-09-07 16:20:52 +0500

    header-changing

--- src/components/Layouts/PostLoginLayout.tsx
+++ src/components/Layouts/PostLoginLayout.tsx
@@ -5,38 +5,29 @@ import Image from "next/image";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Popover,
   PopoverHandler,
   PopoverContent,
-  Button,
-  Chip,
   Typography,
 } from "@material-tailwind/react";
-
 const inter = Inter({ subsets: ["latin"] });
-
 export default function RootLayout({
-
-
   children,
 }: {
   children: React.ReactNode;
 }) {
   const router = useRouter();
   const [openPopover, setOpenPopover] = useState(false);
-
   const triggers = {
     onMouseEnter: () => setOpenPopover(true),
     onMouseLeave: () => setOpenPopover(false),
   };
   const { data: session } = useSession();
-
   if (!session) {
     router.push("/login");
   }
-
   return (
     <div className={inter.className}>
       <div>
@@ -96,7 +87,6 @@ export default function RootLayout({
                 </svg>
               </button>
             </Link>
-
             <Link href="/Zone">
               <button
                 type="button"
@@ -128,7 +118,6 @@ export default function RootLayout({
                 </svg>
               </button>
             </Link>
-
             <Link href="/DualCam">
               <button
                 type="button"
@@ -160,7 +149,6 @@ export default function RootLayout({
                 </svg>
               </button>
             </Link>
-
             <Link href="/Reports">
               <button
                 type="button"
@@ -186,35 +174,8 @@ export default function RootLayout({
                 </svg>
               </button>
             </Link>
-
-            {/* <Menu placement="right">
-                <MenuHandler className='border-none'>
-                  <button
-                    type="button"
-                    data-te-toggle="tooltip"
-                    data-te-placement="right"
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                    title="Dual Camera"
-                  >
-                    <svg className="w-20 h-14 py-3  border-y-2 -my-2  text-[white]  text-white-10  dark:text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="7" cy="17" r="2" />  <circle cx="17" cy="17" r="2" />  <path d="M5 17h-2v-6l2-5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5" /></svg>
-                  </button>
-                </MenuHandler>
-
-                <MenuList  >
-                  <Link href="/DriverProfile">
-                    <MenuItem className="mb-5 text-[#00B56C]" >Driver Profile</MenuItem>
-                  </Link>
-
-                  <Link href="/DriverAssign">
-                    <MenuItem className="text-[#00B56C]">Assign Deriver</MenuItem>
-                  </Link>
-                </MenuList>
-
-              </Menu> */}
           </div>
           <hr></hr>
-
           <div className="basis-1/1 w-screen">
             <nav className="flex items-center justify-between flex-wrap bg-gray-50 p-4">
               <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -241,103 +202,40 @@ export default function RootLayout({
                       &nbsp; 8/30/2023, 2:25:59 PM
                     </span>
                   </a>
-
-
                 </div>
                 <div>
                   <a
                     href="#"
                     className="inline-block text-sm px-4 py-2 leading-none lg:mt-0"
                   >
-
-
-
                   </a>
-
                   <Popover open={openPopover} handler={setOpenPopover}>
                     <PopoverHandler {...triggers}>
-                      <img
-                        className="mr-6 inline-block h-10 -my-4 w-10 rounded-full ring-2 ring-white"
-                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <img className=" cursor-pointer -mt-6 w-10 h-10 rounded-full" src="https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg" alt="Rounded avatar" />
                     </PopoverHandler>
-                    <PopoverContent {...triggers} className="z-50 max-w-[26rem]">
-                      <div className="mb-2 flex items-center gap-3">
+                    <PopoverContent {...triggers} className="z-50 w-80 ">
+                      <div className="mb-2 flex items-center gap-3 px-20">
                         <Typography
                           as="a"
                           href="#"
                           variant="h6"
                           color="blue-gray"
-                          className="font-medium transition-colors hover:text-gray-900"
+                          className="font-medium transition-colors hover:text-gray-900 w-full"
                         >
-                          <img
-                            className="ms-auto mr-auto mt-5 mb-5 w-10 h-10 rounded-fullrounded-full ring-2 ring-white"
-                            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                          />
+                          <img className="ms-auto mr-auto mt-5 mb-5 w-10 h-10 rounded-full" src="https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg" alt="Rounded avatar" />
                         </Typography>
-                        <Chip
-                          value="Public"
-                          className="rounded-full py-1 px-2 font-medium capitalize tracking-wide"
-                        />
                       </div>
-                      <Typography variant="small" color="gray" className="font-normal">
-                        @material-tailwind is an easy-to-use components library for Tailwind
-                        CSS and Material Design.
+                      <Typography variant="small" color="gray" className="font-normal ">
+                        <p className="text-2xl mb-3 text-center">Bilal Hussain</p><hr></hr>
+                        <div className="flex justify-center">
+                          <button className="bg-[#00B56C] px-5 py-3 text-white mt-5" onClick={() => {
+                            signOut();
+                          }}>Sighn Out</button>
+                        </div>
                       </Typography>
-                      <div className="mt-4 flex items-center gap-5">
-                        <div className="flex items-center gap-1">
-                          <span className="h-3 w-3 rounded-full bg-blue-700" />
-                          <Typography color="gray" className="text-xs font-normal">
-                            TypeScript
-                          </Typography>
-                        </div>
-                        <div className="flex items-center gap-1">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="-mt-0.5 h-4 w-4 text-yellow-700"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          <Typography color="gray" className="text-xs font-normal">
-                            1,480
-                          </Typography>
-                        </div>
-                        <div className="flex items-center gap-1">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="-mt-px h-4 w-4 text-green-500"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          <Typography color="gray" className="text-xs font-normal">
-                            Veritied
-                          </Typography>
-                        </div>
-                      </div>
                     </PopoverContent>
                   </Popover>
                 </div>
-                {/* <button
-                  onClick={() => {
-                    signOut();
-                  }}
-                >
-                  Logout
-                </button> */}
               </div>
             </nav>
             {children}
