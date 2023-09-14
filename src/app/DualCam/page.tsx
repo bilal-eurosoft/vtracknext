640e13730f440bf057040b42702e3c93eb8284c4
Bilal Hussain <bilal.hussain@eurosofttech.co.uk>
2023-09-14 11:11:22 +0500

    pull addZone

--- src/app/DualCam/page.tsx
+++ src/app/DualCam/page.tsx
@@ -11,39 +11,19 @@ export default function DualCam() {
       <div className="grid lg:grid-cols-2  md:grid-cols-4  px-4 text-start">
         <div className="lg:col-span-1 md:col-span-3  py-5">
           <div className="grid lg:grid-cols-12 md:grid-cols-12 gap-3">
-            <div className="lg:col-span-2 md:col-span-3">
-              <label className=" text-sm text-[#00B56C]">From</label>
-              <br></br>
-              <input
-                type="date"
-                className="mr-1 outline-none border-b-2 border-gray-400"
-                name="bilal"
-              />
-            </div>
 
-            <div className="lg:col-span-2 md:col-span-2">
-              <label className=" text-sm text-[#00B56C]">To</label>
-              <br></br>
-              <input
-                type="date"
-                className="mr-1 outline-none border-b-2 border-gray-400"
-                name="bilal"
-              />
-            </div>
 
-            <div className="lg:col-span-3 md:col-span-2 mt-7"></div>
-
-            <div className="lg:col-span-1 md:col-span-2 sm:col-span-2 mt-5">
+            <div className="lg:col-span-2 md:col-span-2 sm:col-span-2 mt-5">
               <input type="radio" className="w-3 h-3" name="bilal" />
               <label className="text-sm "> Both</label>
             </div>
 
-            <div className="lg:col-span-1 md:col-span-2 sm:col-span-2 mt-5">
+            <div className="lg:col-span-2 md:col-span-2 sm:col-span-2 mt-5">
               <input type="radio" className="w-3 h-3  " name="bilal" />
               <label className="text-sm "> photo</label>
             </div>
 
-            <div className="lg:col-span-1 md:col-span-2  sm:col-span-2 mt-5">
+            <div className="lg:col-span-2 md:col-span-2  sm:col-span-2 mt-5">
               <input type="radio" className="w-3 h-3" name="bilal" />
               <label className="text-sm "> video</label>
             </div>
@@ -58,18 +38,68 @@ export default function DualCam() {
 
         <div className="col-span-1"></div>
       </div>
-      <div className="grid lg:grid-cols-5  sm:grid-cols-5 md:grid-cols-5 grid-cols-1">
-        <div className="lg:col-span-1 md:col-span-1 sm:col-span-4  col-span-4 bg-white">
+      <div className="grid lg:grid-cols-5  sm:grid-cols-5 md:grid-cols-5 grid-cols-1" style={{ height: '46em' }}>
+        <div className="lg:col-span-1 md:col-span-1 sm:col-span-4  col-span-4 bg-bgLight">
           <p className="bg-[#00B56C] px-4 py-1 text-white">Tips(0)</p>
+          <div className="grid lg:grid-cols-3 text-center mt-5">
+            <div className="lg:col-span-1">
+              <p className="text-red font-bold">Abu-878</p>
+            </div>
+            <div className="lg:col-span-1">
+              <button className="text-white bg-red p-1 -mt-1 shadow-lg">
+                Parked
+              </button>
+            </div>
+            <div className="col-span-1">
+              <div className="grid grid-cols-4">
+                <div className="lg:col-span-3 col-span-2">
+                  23Mph
+                </div>
+                <div className="lg:col-span-1">
+                  <svg
+                    className={`h-6 w-3 text-red mr-2`}
+                    viewBox="0 0 24 24"
+                    fill='red'
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="10" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
 
         <div className="lg:col-span-4  md:col-span-4  sm:col-span-5 col-span-4 ">
-          <iframe
-            style={{ height: "45.8em" }}
-            className="w-full  overflow-hidden"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14480.14584461658!2d67.01494291377836!3d24.86260426589045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e6d06bea525%3A0xca5759c73e8b99ce!2sSaddar%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1693470315983!5m2!1sen!2s"
-            loading="lazy"
-          ></iframe>
+
+          <h1 className="text-center text-5xl font-serif mt-20">Image</h1><hr className="w-36 ms-auto mr-auto"></hr>
+          <div className="grid grid-cols-3 text-center mt-10 gap-10 mx-5">
+            <div className="col-span-1">
+              <a>It is a long established fact that a reader will be distracted by the readable content of a page</a>
+            </div>
+            <div className="col-span-1">
+              <a>It is a long established fact that a reader will be distracted by the readable content of a page</a>
+            </div>
+            <div className="col-span-1">
+              <a>It is a long established fact that a reader will be distracted by the readable content of a page</a>
+            </div>
+          </div>
+
+          <h1 className="text-center text-5xl font-serif mt-20">Video</h1><hr className="w-36 ms-auto mr-auto"></hr>
+          <div className="grid grid-cols-3 text-center mt-5 gap-10 mx-5">
+            <div className="col-span-1">
+              <a>It is a long established fact that a reader will be distracted by the readable content of a page</a>
+            </div>
+            <div className="col-span-1">
+              <a>It is a long established fact that a reader will be distracted by the readable content of a page</a>
+            </div>
+            <div className="col-span-1">
+              <a>It is a long established fact that a reader will be distracted by the readable content of a page</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
