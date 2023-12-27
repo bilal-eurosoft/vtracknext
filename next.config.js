42d32f3de085ae813433e08916bde47e6570a558
bilal hussain <bilal.hussain@eurosofttech.co.uk>
2023-12-27 14:22:43 +0500

    changing-dependency

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "exports",
+  output: "export",
   experimental: {},
   images: {
     domains: ["vtracksolutions.s3.eu-west-2.amazonaws.com"],
